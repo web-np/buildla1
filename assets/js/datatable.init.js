e574266aebc9b0989dcde1a219a4f37138cb6109
Vishal Thakkar <vishal.thakkar@3insys.com>
2022-12-10 22:18:34 +0530

    update

---
+++ assets/js/datatable.init.js
@@ -0,0 +1,3 @@
+﻿$(document).ready(function () {
+    $("[data-Table]").DataTable(), $("#datatable-buttons").DataTable({ lengthChange: !1, buttons: ["copy", "excel", "pdf", "colvis"] }).buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)"), $(".dataTables_length select").addClass("form-select form-select-sm")
+});
---
+++ assets/js/layout.js
@@ -0,0 +1,284 @@
+﻿const header = `<div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="header-left">
+                        <div class="d-flex align-items-center">
+                            <div class="my-2">
+                                <img class="d-inline-flex" src="assets/img/logo/lacity_logo.svg" style="height:35px;"><span class="text-white d-inline-flex fw-bold mx-1" style="font-family: 'Oswald', sans-serif !important;">LOS ANGELES</span>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="header-right">
+                        <div class="d-flex align-items-center mx-1">
+                            <div class="">
+                                <i class="fas fa-virus-covid text-white"></i>
+                                <a href="#" class="text-white">Update on coronavirus</a>
+                            </div>
+                            <div class="vr bg-white mx-2"></div>
+                            <div class="">
+                                <span class="bg-white rounded rounded-2 p-1" style="color:var(--theme-color-one);">311</span>
+                                <a href="#" class="text-white">City Services</a>
+                            </div>
+                            <div class="vr bg-white mx-2"></div>
+                            <div class="">
+                                <span class="bg-white rounded rounded-2 p-1" style="color:var(--theme-color-one);">LA</span>
+                                <a href="#" class="text-white">City Directory</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div> `;
+const footer = `<div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="footer-widget">
+                        <div class="footer-logo">
+                            <a href="index.html">
+                                <img src="assets/img/logo/BuildLA_Logo.png" style="height: 48px;" class="footer-logo-one py-1" alt="Logo">
+                                <!-- <img src="assets/img/logo-2.png" class="footer-logo-two" alt="Logo"> -->
+                            </a>
+                        </div>
+                        <p>
+                            Provides immediate responses to all calls requiring non-technical assistance
+                        </p>
+
+                    </div>
+                </div>
+
+
+                <div class="col-lg-4 col-sm-6 col-6">
+                    <div class="footer-widget">
+                        <h3>Links</h3>
+                        <ul class="footer-list">
+                            <li>
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">Locations</a>
+                            </li>
+                            <li>
+                                <a href="#">Tools</a>
+                            </li>
+                            <li>
+                                <a href="#">All Services</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 col-6">
+                    <div class="footer-widget">
+                        <h3>Locations</h3>
+                        <ul class="footer-list-two">
+                            <li>
+                                <a href="https://www.google.com/maps/place/LADBS/@34.058761,-118.2515765,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x3d23c8d1fbedaf5!8m2!3d34.058761!4d-118.2515765" target="_blank">
+                                    <i class='bx bx-location-plus'></i>
+                                    201 N Figueroa St
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.google.com/maps?q=6262+Van+Nuys+Blvd,+Van+Nuys,+CA+91401,+USA&ftid=0x80c29706f2a8165b:0xf8ad98c560f35b7b" target="_blank">
+                                    <i class='bx bx-location-plus'></i>
+                                    6262 Van Nuys Blvd
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.google.com/url?q=https://maps.google.com/?q%3D1828%2BSawtelle%2BBlvd,%2BLos%2BAngeles,%2BCA%2B90025,%2BUSA%26ftid%3D0x80c2bb76ac95d6b9:0x40b5edc4d4c6da2f&sa=D&sntz=1&usd=2&usg=AOvVaw0c5zNuNq7jr7u-DrC2qA5H" target="_blank">
+                                    <i class='bx bx-location-plus'></i>
+                                    1828 Sawtelle Blvd
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.google.com/maps?q=1968+W+Adams+Blvd,+Los+Angeles,+CA+90018,+USA&ftid=0x80c2b87320bce11b:0x1d40f458a2661d53" target="_blank">
+                                    <i class='bx bx-location-plus'></i>
+                                    1968 W Adams Blvd
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.google.com/maps?q=San+Pedro+Municipal+Building,+638+S+Beacon+St+%23276,+San+Pedro,+CA+90731,+USA&ftid=0x80dd3636e1dadbb7:0x5386f5fb9851187a" target="_blank">
+                                    <i class='bx bx-location-plus'></i>
+                                    638 S Beacon St #276
+                                </a>
+                            </li>
+
+                        </ul>
+                        <!-- <h3>Contacts</h3> -->
+                        <!-- <ul class="footer-list-two">
+                            <li>
+                                <i class='bx bx-time'></i>
+                                Sun - Fri: 8.00am - 6.00pm
+                            </li>
+                            <li>
+                                <i class='bx bx-location-plus'></i>
+                                6th Floor, Barbosa, Sydney
+                            </li>
+                            <li>
+                                <i class='bx bx-phone'></i>
+                                <a href="tel:+61283171380">+61283171380</a>
+                            </li>
+                            <li>
+                                <i class='bx bx-phone'></i>
+                                <a href="tel:+61483471680">+61483471680</a>
+                            </li>
+                            <li>
+                                <i class='bx bxs-envelope'></i>
+                                <a
+                                    href="https://templates.hibootstrap.com/cdn-cgi/l/email-protection#60090e060f2006090e05034e030f0d"><span
+                                        class="__cf_email__"
+                                        data-cfemail="afc6c1c9c0efc9c6c1cacc81ccc0c2">[email&#160;protected]</span></a>
+                            </li>
+                        </ul> -->
+
+                    </div>
+                </div>
+                <!-- <div class="col-lg-3 col-md-6">
+                    <div class="footer-widget pl-2">
+                        <h3>Newsletter</h3>
+                        <div class="newsletter-area">
+                            <form class="newsletter-form" data-toggle="validator" method="POST">
+                                <input type="email" class="form-control" placeholder="Your Email*" name="EMAIL" required
+                                    autocomplete="off">
+                                <button class="default-btn" type="submit">
+                                    Subscribe
+                                </button>
+                                <div id="validator-newsletter" class="form-result"></div>
+                            </form>
+                        </div>
+                    </div>
+                </div> -->
+            </div>
+        </div>`;
+const menu = `
+        <div class="mobile-nav">
+            <a href="index.html" class="logo py-1">
+                <img src="assets/img/logo/BuildLA_Logo.png" class="logo-one" alt="Logo">
+                <!-- <img src="assets/img/logo-2.png" class="logo-two" alt="Logo"> -->
+            </a>
+        </div>
+
+        <div class="main-nav">
+            <div class="container">
+                <nav class="navbar navbar-expand-md navbar-light ">
+                    <a class="navbar-brand" href="index.html">
+                        <img src="assets/img/logo/BuildLA_Logo.png" style="height: 48px;" class="logo-one py-1" alt="Logo">
+                        <!-- <img src="assets/img/logo-2.png" class="logo-two" alt="Logo"> -->
+                    </a>
+                    <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+                        <ul class="navbar-nav m-auto">
+                            <li class="nav-item">
+                                <a href="index.html" class="nav-link">
+                                    Home
+                                    <!-- <i class='bx bx-chevron-down'></i> -->
+                                </a>
+
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    Locations
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    Tools
+                                    <!-- <i class='bx bx-chevron-down'></i> -->
+                                </a>
+
+                            </li>
+                            <li class="nav-item">
+                                <a href="service.html" class="nav-link">
+                                    All Services
+                                    <!-- <i class='bx bx-chevron-down'></i> -->
+                                </a>
+
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    Contact Us
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="others-options d-flex align-items-center">
+                            <div class="option-item">
+                                <i class='search-btn bx bx-search'></i>
+                                <i class='close-btn bx bx-x'></i>
+                                <div class="search-overlay search-popup">
+                                    <div class='search-box'>
+                                        <form class="search-form">
+                                            <input class="search-input" name="search" placeholder="Search" type="text">
+                                            <button class="search-button" type="submit">
+                                                <i class="bx bx-search"></i>
+                                            </button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="option-item">
+                                <div class="menu-icon d-in-line">
+                                    <a href="#" class="burger-menu menu-icon-two">
+                                        <i class='flaticon-menu'></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <div class="side-nav-responsive">
+            <div class="container">
+                <div class="dot-menu">
+                    <div class="circle-inner">
+                        <div class="circle circle-one"></div>
+                        <div class="circle circle-two"></div>
+                        <div class="circle circle-three"></div>
+                    </div>
+                </div>
+                <dziv class="container">
+                    <div class="side-nav-inner">
+                        <div class="side-nav justify-content-center align-items-center">
+                            <div class="side-item">
+                                <div class="option-item">
+                                    <div class="menu-icon d-in-line">
+                                        <a href="#" class="burger-menu menu-icon-two">
+                                            <i class='flaticon-menu'></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="side-item">
+                                <div class="option-item">
+                                    <i class='search-btn bx bx-search'></i>
+                                    <i class='close-btn bx bx-x'></i>
+                                    <div class="search-overlay search-popup">
+                                        <div class='search-box'>
+                                            <form class="search-form">
+                                                <input class="search-input" name="search" placeholder="Search"
+                                                       type="text">
+                                                <button class="search-button" type="submit">
+                                                    <i class="bx bx-search"></i>
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>`;
+(function () {
+
+    document.querySelector('[data-navbar-area]').innerHTML = menu;
+    document.querySelector('header').innerHTML = header;
+    document.querySelector('footer').innerHTML = footer;
+
+})();
